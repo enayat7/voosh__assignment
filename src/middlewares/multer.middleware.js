9c732314e14a125c807dc53007178c3f5129debb
enayat7 <99501809+enayat7@users.noreply.github.com>
2024-05-25 23:04:03 +0530

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer"
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       // console.log(4567345)
-      cb(null, "../public")
+      cb(null, "./public")
     },
     filename: function (req, file, cb) {
       return cb(null,` ${Date.now()} + '-' + ${file.originalname}`)
